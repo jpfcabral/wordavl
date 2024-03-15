adeb5d78cc8c8ca1b582fb94e1d12e859b13e30e
João Pedro <jpfcabral@gmail.com>
2024-03-15 12:11:08 -0300

    add autocomplete in recursive way

--- wordavl/core.py
+++ wordavl/core.py
@@ -1,5 +1,6 @@
 from loguru import logger
 
+from wordavl.structures.avl import AVLNode
 from wordavl.structures.avl import AVLTree
 from wordavl.text_data import TextData
 
@@ -37,3 +38,53 @@ class WordAVL(AVLTree):
             self.add(word)
 
         logger.info(f"AVL population completed with height: {self.get_height()}")
+
+    def autocomplete(self, prefix: str):
+        """
+        Provide autocomplete suggestions based on the given prefix.
+
+        Args:
+            prefix (str): The prefix for which autocomplete suggestions are requested.
+
+        Returns:
+            str: A string containing autocomplete suggestions based on the provided prefix.
+                Returns an empty string if no prefix is provided.
+        """
+        if not prefix or prefix == "":
+            return ""
+
+        return self._find_all_elements_with_prefix(prefix=prefix)
+
+    def _find_all_elements_with_prefix(self, prefix: str):
+        """
+        Find all elements in the AVL tree with the given prefix.
+
+        Args:
+            prefix (str): The prefix to search for.
+
+        Returns:
+            list: A list containing all elements in the AVL tree with the given prefix.
+        """
+        results = list()
+
+        for word in self._find_recursive(self.root, prefix):
+            results.append(word)
+
+        return results
+
+    def _find_recursive(self, current_node: AVLNode, prefix: str):
+        """
+        Recursively find all node values with the given prefix starting from the current node.
+        """
+
+        # Check if the current node value starts with the prefix
+        if current_node is not None and current_node.value.startswith(prefix):
+            yield current_node.value
+
+        # Recursively search in the left subtree
+        if current_node is not None:
+            yield from self._find_recursive(current_node.left_child, prefix)
+
+        # Recursively search in the right subtree
+        if current_node is not None:
+            yield from self._find_recursive(current_node.right_child, prefix)
